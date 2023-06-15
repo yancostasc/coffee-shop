d4002c2e67cb61ccdd7dc66ffc9109d8a8fccadf
Yan <yan.costa@projuris.com.br>
2023-06-15 04:20:59 -0300

    feat: adding scrollDown function to the try now button

--- src/app/nav-header/nav-header.component.ts
+++ src/app/nav-header/nav-header.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav-header',
   templateUrl: './nav-header.component.html',
   styleUrls: ['./nav-header.component.scss'],
 })
-export class NavHeaderComponent {}
+export class NavHeaderComponent {
+  constructor(private elementRef: ElementRef) {}
+
+  scrollDown() {
+    const element = this.elementRef.nativeElement;
+    element.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }
+}
